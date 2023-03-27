78fe0a804b4071e3d0b65574eb2d1f9d98c27b6d
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-26 23:00:39 -0700

    changed single quotes to double quotes in 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # puppet ssh config file
-$updates = '\nPasswordAuthentication no\nIdentityFile ~/.ssh/school\n'
+$updates = "PasswordAuthentication no \n IdentityFile ~/.ssh/school \n"
 $file = file('/etc/ssh/ssh_config')
 $content = "${file}${updates}"
 file { '/etc/ssh/ssh_config':
