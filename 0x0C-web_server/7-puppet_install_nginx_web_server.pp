e34069c8ce85327bc70a49a039fceabd6e3f502f
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-30 05:01:14 -0700

    crxn 1 for 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,14 @@
 # install and set up nginx server
 
-exec { 'install nginx':
-  command   => 'sudo apt-get -y install nginx'
+sh = "/usr/env/bash"
+new="\"server_name _;\n\t location \/redirect_me {\n\treturn 301 https\:\/\/www.google.com;\n\t}\""
+cntnt = "#!${sh} \nsudo apt-get -y update; sudo apt-get -y install nginx && sudo ufw allow 'Nginx HTTP'; sudo service nginx start; if [[ ! -d /var/www/html ]];then\n sudo ${sh}/mkdir /var/www/html\nfi\n sudo ${sh}/chmod 0666 /var/www/html; sudo ${sh}/touch /var/www/html/index.html && sudo ${sh}/chmod 0777 /var/www/html/index.html && ${sh}/echo 'Hello World!'>/var/www/html/index.html; sudo ${sh}/chmod a+wrx /etc/nginx/sites-available/ && sudo ${sh}/chmod a+wrx /etc/nginx/sites-available/default; sudo ${sh}/chmod a+wrx /etc/nginx/sites-enabled/default;  sed -i \"s/server_name _;/${new}/\" /etc/nginx/sites-available/default; sudo service nginx reload "
+file { 'nginx_setup':
+  path   => '~/',
+  ensure => file,
+  content => $cntnt,
+  mode => '0777'
 }
-exec { 'set up firewall':
-  command  => "sudo ufw allow 'Nginx HTTP'"
-}
-exec { 'create index.html':
-  command => 'sudo touch /var/www/html/index.html'
-}
-exec { 'change mode':
-  command => 'sudo chmod 0777 /var/www/html/index.html'
-}
-exec { 'input text':
-  command => "echo 'Hello World!'>/var/www/html/index.html"
-}
-exec { 'redirect: chmod':
-  command => 'sudo chmod a+wrx /etc/nginx/sites-available'
-}
-exec { 'redirect: string':
-  command => "new='server_name _;\n\t location \/redirect_me {\n\treturn 301 https\:\/\/www.google.com;\n\t}'"
-}
-exec { 'redirect: sed':
-  command => "sed -i 's/server_name _;/${new}/' /etc/nginx/sites-available/default && sudo service nginx reload"
+exec { 'setup_server':
+  command  => "./root/nginx_setup"
 }
