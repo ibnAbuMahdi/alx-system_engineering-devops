4dabed30b89707510c48274930d29381d78175f1
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-30 05:11:42 -0700

    crxn 3 for 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # install and set up nginx server
 
-sh = "/usr/env/bash"
+sh = "/usr/bin/bash"
 new="\"server_name _;\n\t location \/redirect_me {\n\treturn 301 https\:\/\/www.google.com;\n\t}\""
 cntnt = "#!${sh} \nsudo apt-get -y update; sudo apt-get -y install nginx && sudo ufw allow 'Nginx HTTP'; sudo service nginx start; if [[ ! -d /var/www/html ]];then\n sudo ${sh}/mkdir /var/www/html\nfi\n sudo ${sh}/chmod 0666 /var/www/html; sudo ${sh}/touch /var/www/html/index.html && sudo ${sh}/chmod 0777 /var/www/html/index.html && ${sh}/echo 'Hello World!'>/var/www/html/index.html; sudo ${sh}/chmod a+wrx /etc/nginx/sites-available/ && sudo ${sh}/chmod a+wrx /etc/nginx/sites-available/default; sudo ${sh}/chmod a+wrx /etc/nginx/sites-enabled/default;  sed -i \"s/server_name _;/${new}/\" /etc/nginx/sites-available/default; sudo service nginx reload "
 file { 'nginx_setup':
