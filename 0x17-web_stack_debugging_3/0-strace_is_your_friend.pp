58ea9937cc6bba4d74067c77a4f5ec2e02998e97
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-05-11 09:40:45 +0000

    crxn 2 for strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 file { '/var/www/html/wp-settings.php':
   ensure  => file,
-  content => regsubst(file("/var/www/html/wp-settings.php"), 'class-wp-locale.phpp', 'class-wp-locale.php')
+  content => regsubst(file('/var/www/html/wp-settings.php'), 'class-wp-locale.phpp', 'class-wp-locale.php')
 }
